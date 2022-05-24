8aaa36180c5e4c071be68cad17c9c35eced0bdd3
sungsikyang92 <sungsik.yang92@gmail.com>
2022-05-24 22:14:12 +0900

    Update code readability

--- src/main/java/org/edwith/webbe/guestbook/dao/GuestbookDao.java
+++ src/main/java/org/edwith/webbe/guestbook/dao/GuestbookDao.java
@@ -12,64 +12,58 @@ import java.util.Date;
 import java.util.List;
 
 public class GuestbookDao {
-//	DBUtil db = new DBUtil();
+
     public List<Guestbook> getGuestbooks(){
         List<Guestbook> list = new ArrayList<>();
 
-        // 코드를 작성하세요.
     	try {
 			Class.forName("com.mysql.cj.jdbc.Driver");
 		} catch (ClassNotFoundException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
     	
     	String sql = "SELECT id, name, content, regdate from guestbook order by id";
     	
     	try (Connection conn = DBUtil.getConnection();
-    			PreparedStatement ps = conn.prepareStatement(sql);){
-			try(ResultSet rs = ps.executeQuery()){
-				while(rs.next()) {
-					Long id = rs.getLong(1);
-					System.out.println(id);
-					String name = rs.getString(2);
-					String content = rs.getString(3);
-					Date regdate = rs.getDate(4);
-					
-					Guestbook guestbook = new Guestbook(id, name, content, regdate);
-					list.add(guestbook);
-				}
-			} catch (Exception e) {
-				e.printStackTrace();
+			PreparedStatement ps = conn.prepareStatement(sql);
+			){
+		try(ResultSet rs = ps.executeQuery())
+		{
+		while(rs.next()) 
+		{
+			Long id = rs.getLong(1);
+			String name = rs.getString(2);
+			String content = rs.getString(3);
+			Date regdate = rs.getDate(4);
+				
+			Guestbook guestbook = new Guestbook(id, name, content, regdate);
+			list.add(guestbook);
 			}
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
-    	System.out.println("추가완");
+    	} catch (SQLException e) {
+			e.printStackTrace();
+		}
         return list;
     }
 
     public void addGuestbook(Guestbook guestbook){
-        // 코드를 작성하세요.
     	try {
 			Class.forName("com.mysql.cj.jdbc.Driver");
 		} catch (ClassNotFoundException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
     	
     	String sql = "INSERT INTO guestbook (id, name, content, regdate) VALUES (id, ?, ?, now())";
  
     	try (Connection conn = DBUtil.getConnection();
-    			PreparedStatement ps = conn.prepareStatement(sql);){
+			PreparedStatement ps = conn.prepareStatement(sql);){
 			ps.setString(1, guestbook.getName());
 			ps.setString(2, guestbook.getContent());
 			ps.executeUpdate();
 		} catch (SQLException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-    	System.out.println("추가DAO");
     }
 }
--- src/main/java/org/edwith/webbe/guestbook/servlet/GuestbookListServlet.java
+++ src/main/java/org/edwith/webbe/guestbook/servlet/GuestbookListServlet.java
@@ -19,19 +19,12 @@ import javax.servlet.http.HttpServletResponse;
 public class GuestbookListServlet extends HttpServlet {
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        // 코드를 작성하세요.
+
     	response.setContentType("text/html;charset=UTF-8");
     	
     	GuestbookDao guestbookDao = new GuestbookDao();
-    	System.out.println("dao 만들어지나요?" + guestbookDao);
     	List<Guestbook> booklist = guestbookDao.getGuestbooks();
 
-    	System.out.println("방명록 가져오나");
-    	for(int i = 0; i<booklist.size(); i++) {
-    		for(Guestbook gb : booklist) {
-    			System.out.println(gb);
-    		}
-    	}
     	request.setAttribute("booklist", booklist);
     	
     	request.getRequestDispatcher("guestbooks.jsp").forward(request, response);
